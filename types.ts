cc1567a6ac6346d41b714e5a5f65c2773f1af6f7
rodrigofbmm <115274224+rodrigofbmm@users.noreply.github.com>
2023-10-27 12:56:58 +0200

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,8 @@
+export type Person = {
+    name: string;
+    dni: string;
+    age: number;
+    id: string;
+  };
+
+  //mongodb+srv://rodrigofbm:1234567890r@cluster0.ern9y.mongodb.net/ejemploclase?retryWrites=true&w=majority
